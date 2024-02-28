bf3381b7f560d5142ab85f5e4fd7e7a914e5ed5b
Dreamy-Z3r0 <danielblake98vt@gmail.com>
2024-02-28 20:29:58 +0700

    update parse_xml methods

--- parse_xml.py
+++ parse_xml.py
@@ -1,25 +1,33 @@
-def parse_xml():
+def parse_xml(keyValue):
     try:
-        xml_file = open('./directives.xml', 'rt')
-        xml_file_contents = xml_file.read()
-        xml_file.close()
+        XML_file = open('./directives.xml', 'rt')
+        XML_fileContents = XML_file.read()
+        XML_file.close()
+
+        delimiterIndex = keyValue.index('/')
+        while (delimiterIndex >= 0):
+            lookupValue = keyValue[:delimiterIndex]
+            keyValue = keyValue[(delimiterIndex+1):]
+            XML_fileContents = check_key(XML_fileContents, lookupValue)
+
+        return XML_fileContents
 
     except:
         print("Directive file is missing.")
 
-def check_key(xml_file_contents, key_value, range=None):
+def check_key(XML_fileContents, keyValue, range=None):
     startPos = 0
-    endPos = len(xml_file_contents) - 1
+    endPos = len(XML_fileContents) - 1
 
     if not(range is None):
         try:
             startPos = range[0]
             endPos = range[1]
 
-            if ((-1 == endPos) | ((len(xml_file_contents) - 1) == endPos)):
-                xml_file_contents = xml_file_contents[startPos:]
+            if ((-1 == endPos) | ((len(XML_fileContents) - 1) == endPos)):
+                XML_fileContents = XML_fileContents[startPos:]
             else:
-                xml_file_contents = xml_file_contents[startPos:endPos+1]
+                XML_fileContents = XML_fileContents[startPos:endPos+1]
                 
         except:
             print(f"Invalid XML look-up parameter: range = {range}")
@@ -27,17 +35,19 @@ def check_key(xml_file_contents, key_value, range=None):
             return 0
 
     keyPair = []
-    keyPair.append('<' + key_value + '>')
-    keyPair.append('</' + key_value + '>')
+    keyPair.append('<' + keyValue + '>')
+    keyPair.append('</' + keyValue + '>')
 
-    key_startPos = xml_file_contents.index(keyPair[0]) + len(keyPair[0]) + 1
-    key_endPos = xml_file_contents.index(keyPair[1]) - 1
+    key_startPos = XML_fileContents.index(keyPair[0]) + len(keyPair[0]) + 1
+    key_endPos = XML_fileContents.index(keyPair[1]) - 1
 
     validityFlag = (key_startPos >= 0) & (key_endPos >= 0)
     validityFlag &= (key_startPos < key_endPos)
 
-    if not(validityFlag)
+    if not(validityFlag):
         return False
 
-    key_value = xml_file_contents[key_startPos : key_endPos]
-    return key_value
+    keyValue = XML_fileContents[key_startPos : key_endPos]
+    return keyValue
+
+if __name__ == '__main__':
